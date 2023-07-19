c4dae0499ae6409314a1ecac3e5aec7b1c4d5234
LealNick <139706091+LealNick@users.noreply.github.com>
2023-07-18 21:17:21 -0300

    Mudanças e adições:
    - Adicionados os novos botões: "<" e ".";
    - As bordas agora são menos arredondadas;
    - Altura do visor aumentada;
    - Sinais "/" e "*" substituídos por "÷" e "×";
    - Agora os cálculos podem ser feitos diretamente pelo teclado.

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-function insert(num){
+function insert(num) {
     var number = document.getElementById('view').innerHTML;
-    document.getElementById('view').innerHTML = number + num;
+    var symbol = num;
+  
+    if (num === '*') {
+      symbol = '×';
+    }
+    else if (num === '/') {
+        symbol = '÷';
+    }
+  
+    document.getElementById('view').innerHTML = number + symbol;
 }
 function clean(){
     document.getElementById('view').innerHTML = "";
 }
-function calculate(){
+function back(){
     var result = document.getElementById('view').innerHTML;
-    if(result){
+    document.getElementById('view').innerHTML = result.substring(0, result.length -1);
+}
+function calculate() {
+    var result = document.getElementById('view').innerHTML;
+  
+    if (result) {
+        result = result.replace(/×/g, '*');
+        result = result.replace(/÷/g, '/');
         document.getElementById('view').innerHTML = eval(result);
-    }
-    else{
-        document.getElementById('view').innerHTML = "nothing...";
+    } 
+    else {
+        document.getElementById('view').innerHTML = 'nothing...';
     }
 }
+document.addEventListener('keydown', function(event) {
+    if (event.key === '*') {
+        insert('×');
+    }
+    else if (event.key === '/') {
+        insert('÷');
+    }
+    else if (/[\d+\-*/\.]/.test(event.key)) {
+        insert(event.key);
+    } 
+    else if (event.key === 'Enter') {
+        calculate();
+    } 
+    else if (event.key === 'Backspace') {
+        back();
+    } 
+    else if (event.key === 'Delete') {
+        clean();
+    }
+});
